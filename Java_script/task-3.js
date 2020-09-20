54ba06bf221d380ac70908e0c056b0cefee9fb78
Aslanov Rustam <aslanovrg@gmail.com>
2020-09-20 17:18:09 +0300

    добавляет решение третьей и четвертой задачи

--- Java_script/task-3.js
+++ Java_script/task-3.js
@@ -7,3 +7,33 @@
 
 // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 // Добавь минимальное оформление галереи флексбоксами или гридами через css - классы.
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const collectionImgEl = images.map((coll) => {
+  const listEl = document.createElement(`li`);
+  const imgEl = document.createElement(`img`);
+  imgEl.src = coll.url;
+  imgEl.alt = coll.alt;
+  listEl.append(imgEl);
+
+  return listEl;
+});
+
+const collectionEl = document.querySelector("#gallery");
+collectionEl.append(...collectionImgEl);
--- Java_script/task-4.js
+++ Java_script/task-4.js
@@ -3,3 +3,26 @@
 // Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+// const counterValue = document.querySelector(`#value`).textContent;
+
+const counterValue = document.querySelector(`#value`);
+const decrementEl = document.querySelector(
+  `#counter button[data-action="decrement"]`
+);
+const incrementEl = document.querySelector(
+  `#counter button[data-action="increment"]`
+);
+decrementEl.addEventListener(`click`, handleButtonDecrClick);
+function handleButtonDecrClick() {
+  let value = counterValue.textContent;
+  value -= 1;
+  counterValue.textContent = value;
+  return counterValue;
+}
+incrementEl.addEventListener(`click`, handleButtonIncrClick);
+function handleButtonIncrClick() {
+  let value = Number(counterValue.textContent);
+  value += 1;
+  counterValue.textContent = value;
+  return counterValue;
+}
